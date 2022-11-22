c69999f822c84d09c4f924b84a02e75918c7d93e
Mustafa Çaylı <90175507+mustafacayli@users.noreply.github.com>
2022-11-23 02:56:02 +0300

    Create yiginkuyruk.c

---
+++ yiginkuyruk.c
@@ -0,0 +1,67 @@
+/******************************************************************************
+
+                            Online C Compiler.
+                Code, Compile, Run and Debug C program online.
+Write your code in this editor and press "Run" button to compile and execute it.
+
+*******************************************************************************/
+
+#include <stdio.h>
+
+int main()
+{
+    
+    int arr[9],sirali[9];
+    
+    
+    printf("1) bireysel\n2)kurumsal\n3)65+ \n1-2-3 \nsayilarindan birini giriniz'");
+    int i,k,j,l;
+    for(i=0;i<10;i++){
+        scanf("%d",&arr[i]);
+    }
+    j=0;
+    for(i=0;i<10;i++){
+        if(arr[i]==3){
+            
+            sirali[j]=arr[i];
+           j+=1; 
+        }
+        
+    }
+    for(i=0;i<10;i++){
+        if(arr[i]!=3){
+            
+            sirali[j]=arr[i];
+           j+=1; 
+        }
+        
+    }
+    for(i=0;i<10;i++){
+        arr[i]=sirali[i];
+    }
+    
+    for(i=0;i<10;i++){
+        
+        if(arr[i]==2&&arr[i-1]==2&arr[i+1]==2&&arr[i+2]==1){
+            sirali[i]=2;
+            sirali[i-1]=2;
+            sirali[i+1]=1;
+            sirali[i+2]=2;
+        }
+        
+        
+    }
+    
+    
+    
+    for(i=0;i<10;i++){
+        printf("%d,",sirali[i]);
+    }
+    
+    
+    
+    
+    
+
+    return 0;
+}
